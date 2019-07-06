68aa9c5df5417d8d15af73f8ea2c4f9d5acb5d1d
jamesbieber <jamesbieber97@gmail.com>
2019-07-06 11:25:55 -0700

    Tests setup, creating endpoints

--- api/server.test.js
+++ api/server.test.js
@@ -5,8 +5,65 @@ describe("server", () => {
   describe("GET /", () => {
     it("responds with 200", () => {
       return supertest(server)
-        .get("/")
+        .get("/games")
         .expect(200);
+    });
+
+    it("returns an array", async () => {
+      await supertest(server)
+        .get("/games")
+        .expect(Array.isArray(res.body))
+        .toBe(true);
+    });
+
+    it("returns empty array if 0 games", async () => {
+      await supertest(server)
+        .get("/games")
+        .expect(res.body)
+        .toEqual([]);
+    });
+
+    it('responds with { api: "up"}', async () => {
+      await supertest(server)
+        .get("/games")
+        .then(res => {
+          expect(res.body).toEqual({ api: "up" });
+        });
+    });
+  });
+
+  describe("POST /", () => {
+    it("responds with 200", async () => {
+      await supertest(server)
+        .post("/games")
+        .send({
+          title: "Pacman",
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(res.status).toBe(200);
+    });
+
+    it("requires a title", async () => {
+      await supertest(server)
+        .post("/games")
+        .send({
+          title: "",
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(res.status).toBe(422);
+    });
+
+    it("requires a genre", async () => {
+      await supertest(server)
+        .post("/games")
+        .send({
+          title: "Pacman",
+          genre: "",
+          releaseYear: 1980
+        });
+      expect(res.status).toBe(422);
     });
   });
 });
